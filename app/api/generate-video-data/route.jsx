37e36443e4ded709c2c6142ffdc023371e0e77dc
Natalia Krasnobaieva <natali@visitorqueue.com>
2025-09-25 13:27:50 +0200

    generated audio

---
+++ app/api/generate-video-data/route.jsx
@@ -0,0 +1,15 @@
+import { inngest } from "@/inngest/client";
+import { NextResponse } from "next/server";
+
+export async function POST(req) {
+  const formData = await req.json();
+
+  const result = await inngest.send({
+    name: 'generate-video-data',
+    data: {
+      ...formData
+    },
+    url: "http://localhost:3000" // порт CLI
+  })
+ return NextResponse.json({ result: result });
+}
