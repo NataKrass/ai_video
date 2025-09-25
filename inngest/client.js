6cb14fc6e2335055b45ef21d427ccdf02cce2652
Natalia Krasnobaieva <natali@visitorqueue.com>
2025-09-25 13:27:50 +0200

    generated audio

--- inngest/client.js
+++ inngest/client.js
@@ -1,4 +1,7 @@
 import { Inngest } from "inngest";
 
 // Create a client to send and receive events
-export const inngest = new Inngest({ id: "ai-video" });
+export const inngest = new Inngest({ 
+  id: "ai-video",
+  url: "http://localhost:3000",
+});
