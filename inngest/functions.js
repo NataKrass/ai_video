813dbc051cdc5551a0d2dd9ad427d94f36df24e7
Natalia Krasnobaieva <natali@visitorqueue.com>
2025-10-17 19:11:42 +0200

    added captions

--- inngest/functions.js
+++ inngest/functions.js
@@ -1,5 +1,7 @@
 import axios from "axios";
 import { inngest } from "./client";
+import { createClient } from "@deepgram/sdk";
+
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
@@ -25,27 +27,46 @@ export const GenerateVideoData = inngest.createFunction(
     const GenerateAudioFile = await step.run(
       "GenerateAudioFile",
       async() => {
-        const result = await axios.post(BASE_URL+'/api/text-to-speech',
-        {
-            input: script,
-            voice: voice
-            // input: 'Sample Audio Text',
-            // voice: 'am_michael'
-        },
-        {
-            headers: {
-                'x-api-key': process.env.AIGURULAB_API_KEY,
-                'Content-Type': 'application/json',
-            },
-        })
-        console.log('audio', result.data.audio) //Output Result: Audio Mp3 Url
-        return result.data.audio;
+        // const result = await axios.post(BASE_URL+'/api/text-to-speech',
+        // {
+        //     input: script,
+        //     voice: voice
+        //     // input: 'Sample Audio Text',
+        //     // voice: 'am_michael'
+        // },
+        // {
+        //     headers: {
+        //         'x-api-key': process.env.AIGURULAB_API_KEY,
+        //         'Content-Type': 'application/json',
+        //     },
+        // })
+        // console.log('audio', result.data.audio) //Output Result: Audio Mp3 Url
+        // return result.data.audio;
+        return 'https://firebasestorage.googleapis.com/v0/b/projects-2025-71366.firebasestorage.app/o/audio%2F1758710931675.mp3?alt=media&token=5303f3d9-81e9-48ad-a850-76f7544c5034'
       }
     )
 
     // Generate Captions 
+    const GenerateCaptions = await step.run(
+      "generateCaptions",
+      async ()=> {
+        const deepgram = createClient(process.env.DEEPGRAM_API_KEY);
 
-    return GenerateAudioFile;
+        const { result, error } = await deepgram.listen.prerecorded.transcribeUrl(
+          {
+            url: GenerateAudioFile,
+          },
+          
+          {
+            model: "nova-3",
+          }
+        );
+        console.log(result)
+        return result.results.channels[0]?.alternatives[0]?.words;
+      }
+    )
+
+    return GenerateCaptions;
    
   }
 )
