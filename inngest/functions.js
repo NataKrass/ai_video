0cbee51b514cef528b5aa1d7bf5a1dc226d0d99a
Natalia Krasnobaieva <natali@visitorqueue.com>
2025-10-21 13:07:43 +0200

    generate img prompt

--- inngest/functions.js
+++ inngest/functions.js
@@ -1,7 +1,18 @@
 import axios from "axios";
 import { inngest } from "./client";
 import { createClient } from "@deepgram/sdk";
+import { GenerateImagePromptScript } from "@/configs/AiModelMain";
 
+const ImagePromptScript = `Generate Image prompt of {style} style with all details for each scene for 30 seconds video : script : {script}
+- Just Give specifing image prompt depends on the story line
+- do not give camera angle image prompt
+- Follow the Following schema and return JSON data (Max 4-5 Images)
+- [
+    {
+      imagePrompt: '',
+      sceneContent: ' <Script Content>'  
+    }
+]`
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
@@ -27,21 +38,21 @@ export const GenerateVideoData = inngest.createFunction(
     const GenerateAudioFile = await step.run(
       "GenerateAudioFile",
       async() => {
-        // const result = await axios.post(BASE_URL+'/api/text-to-speech',
-        // {
-        //     input: script,
-        //     voice: voice
-        //     // input: 'Sample Audio Text',
-        //     // voice: 'am_michael'
-        // },
-        // {
-        //     headers: {
-        //         'x-api-key': process.env.AIGURULAB_API_KEY,
-        //         'Content-Type': 'application/json',
-        //     },
-        // })
-        // console.log('audio', result.data.audio) //Output Result: Audio Mp3 Url
-        // return result.data.audio;
+        const result = await axios.post(BASE_URL+'/api/text-to-speech',
+        {
+            input: script,
+            voice: voice
+            // input: 'Sample Audio Text',
+            // voice: 'am_michael'
+        },
+        {
+            headers: {
+                'x-api-key': process.env.AIGURULAB_API_KEY,
+                'Content-Type': 'application/json',
+            },
+        })
+        console.log('audio', result.data.audio) //Output Result: Audio Mp3 Url
+        return result.data.audio;
         return 'https://firebasestorage.googleapis.com/v0/b/projects-2025-71366.firebasestorage.app/o/audio%2F1758710931675.mp3?alt=media&token=5303f3d9-81e9-48ad-a850-76f7544c5034'
       }
     )
@@ -61,12 +72,23 @@ export const GenerateVideoData = inngest.createFunction(
             model: "nova-3",
           }
         );
-        console.log(result)
         return result.results.channels[0]?.alternatives[0]?.words;
       }
     )
 
-    return GenerateCaptions;
+    //Generate Image Prompt from  script
+    const GenerateImagePrompts = await step.run(
+      "generateImagePrompt",
+      async () => {
+        const FINAL_PROMPT = ImagePromptScript.replace('{style', videoStyle)
+          .replace('{script', script);
+        const result = await GenerateImagePromptScript(videoStyle, script);
+
+        return result;
+      }
+    )
+
+    return GenerateImagePrompts;
    
   }
 )
